ed1b88e8824c396a05cf2bc81aa009738a4f8486
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-07-05 12:18:51 -0300

    primeiro exercicio conversão de variaveis

--- index.php
+++ index.php
@@ -1,8 +1,30 @@
 <?php
 
-$idade = 32;
-$nome = "cesar Szpak";
+$result = "2";
+var_dump($result);
+echo "Resultado exemplo um: " . $result . "<BR>";
 
-echo "O aluno $nome tem " . $idade . " anos.";
 
-?>
+$result_dois = $result + 1;
+var_dump($result_dois);
+echo "Resultado exemplo dois: " . $result_dois . "<BR>";
+
+$result_tres = $result_dois + 3.5;
+var_dump($result_tres);
+echo "Resultado exemplo tres: " . $result_tres . "<BR>";
+
+$result_quatro = 11;
+var_dump($result_quatro);
+echo "Resultado exemplo quatro: " . $result_quatro . "<BR>";
+
+$result_cinco = (double) $result_quatro;
+var_dump($result_cinco);
+echo "Resultado exemplo Cinco: " . $result_cinco . "<BR>";
+
+$result_seis = 7.9;
+var_dump($result_seis);
+echo "Resultado exemplo seis: " . $result_seis . "<BR>";
+
+$result_sete = (int) $result_seis;
+var_dump($result_sete);
+echo "Resultado exemplo sete: " . $result_sete . "<BR>";
