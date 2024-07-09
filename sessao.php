115c866eaf542e9976bf64b6cc200fd8320ecb90
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-07-09 16:35:55 -0300

    SESSAO em php

--- sessao.php
+++ sessao.php
@@ -1,3 +1,8 @@
+<!-- Inicio sessao sempre no topo PHP  -->
+<?php
+    session_start();
+?>
+<!-- Inicio HTML -->
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
@@ -9,8 +14,31 @@
     <body>
         <?php
     
-    // ----SESSÃO
+    // ----CRIAR A SESSÃO - E SALVA NO SERVIDOR
     echo "<h3>SESSÃO</h3>";
+
+    $_SESSION['id'] = 2;
+    $_SESSION['nome'] = "Igor";
+    $_SESSION['sobre'] = "Mota";
+
+    //---VERIFICAR SE EXIXTE A SESSÃO---
+    if(isset($_SESSION['id'])){
+        echo "Id do usuario: " . $_SESSION['id'] . " e o nome é " . $_SESSION['nome'] . " " .  $_SESSION['sobre'] . "<br><br>";
+    } else {
+        echo "Asessão não foi encontrada<br><br>";
+    }
+
+    echo "<hr>";
+    //---EXCLUIR A SESSAO
+    unset($_SESSION['id'], $_SESSION['nome'], $_SESSION['sobre']);
+
+    //---VERIFICAR SE FOI EXCLUIDA---
+    if(isset($_SESSION['id'])){
+        echo "Id do usuario: " . $_SESSION['id'] . " e o nome é " . $_SESSION['nome'] . " " .  $_SESSION['sobre'] . "<br><br>";
+    } else {
+        echo "Asessão não foi encontrada<br><br>";
+    }
+
 
     ?>
     </body>
