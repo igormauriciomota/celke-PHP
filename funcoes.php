b31e919194814367363d12f9d3a0d9b7a43ebb20
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-07-09 13:28:30 -0300

    FUNÇÃO COM PASSAGEM POR VALOR E REFERENCIA

--- funcoes.php
+++ funcoes.php
@@ -49,7 +49,71 @@
 
     $codigo_curso = "cursophp";
     $retorno_dados_funcao = tecnologia();
-    echo $retorno_dados_funcao . "<br>";
+    echo $retorno_dados_funcao . "<br><br>";
+
+    echo "<hr>";
+    // ------PASSAGEM DE PARAMETRO POR VALOR
+    echo "<h4>PASSAGEM POR VALOR</h4>";
+
+    function salario($num){
+        $num += 50;
+        echo "Dentro da função - salario com aumento: R$ " . number_format($num, 2, ",", "." ) . "<br><br>";
+    }
+
+    $salario = 8200;
+    salario($salario);
+    echo "salario sem aumento: R$ " . number_format($salario, 2, ",", "." ) . "<br><br>";
+    echo "<hr>";
+
+    //----------PASSAGEM DE PARAMETRO POR VALOR COM RETURN
+    echo "<h4>PASSAGEM DE PARAMETRO POR VALOR COM RETURN</h4>";
+    function salario_a($num){
+        $num += 100;
+        echo "Dentro da função 1 - salario com aumento: R$ " . number_format($num, 2, ",", "." ) . "<br><br>";
+        return $num;
+    }
+
+    $salario_a = 8500.47;
+    $salario_com_aumento = salario_a($salario_a);
+    echo "Dentro da função 2 - salario sem aumento: R$ " . number_format($salario_a, 2, ",", "." ) . "<br><br>";
+    echo "Dentro da função 3 - salario com aumento: R$ " . number_format($salario_com_aumento, 2, ",", "." ) . "<br><br>";
+    echo "<hr>";
+
+    // PASSAGEM POR REFERENCIA & comercial indica a passagem por referencia, indica tambem que o valor deve ser alterado tambem fora da funçao
+    echo "<h4>Passagem por referencia</h4>";
+
+    function salario_b(&$num){
+        $num += 200;
+        echo "Dentro da função 1 - salario com aumento: R$ " . number_format($num, 2, ",", "." ) . "<br><br>";
+    }
+
+    $salario_b = 9300;
+    echo "Fora da função 2 - salario sem aumento: R$ " . number_format($salario_b, 2, ",", "." ) . "<br><br>";
+    salario_b($salario_b);
+    echo "Fora da função 3 - salario com aumento ao utilizar o &: R$ " . number_format($salario_b, 2, ",", "." ) . "<br><br>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         ?>
