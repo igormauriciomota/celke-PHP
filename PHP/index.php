9dfa7dca341866d6dc4feec7cdd9e02c3613b98d
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-07-24 18:57:19 -0300

    metodo simplificado usando variaveis

--- PHP/index.php
+++ PHP/index.php
@@ -8,9 +8,10 @@ $result_usuarios = mysqli_query($conn, $query_usuarios);
 while ($row_usuario = mysqli_fetch_assoc($result_usuarios)) {
     var_dump($row_usuario);
     extract($row_usuario);
+
     echo "<br><br>";
-    echo "Id. " . $row_usuario['id'] . "<br><br>";
-    echo "Nome: " . $row_usuario['nome'] . "<br><br>";
-    echo "E-mail: " . $row_usuario['email'] . "<br><br>";
+    echo "Id. $id <br><br>";
+    echo "Nome: $nome <br><br>";
+    echo "E-mail: $email <br><br>";
     echo "<hr><br>";
 }
