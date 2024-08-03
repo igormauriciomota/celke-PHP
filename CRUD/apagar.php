a5e3d056b2b9d9c42a02db1dd2e71c83642c44f0
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-08-03 20:51:43 -0300

    Delete CRUD

--- CRUD/apagar.php
+++ CRUD/apagar.php
@@ -10,7 +10,13 @@
     //Conexao BD
     include_once 'conexao.php';
 
-    echo "<h1>Deletar Usuário</h1>";
+    $query_usuario = "DELETE FROM usuarios WHERE id=$id";
+    mysqli_query($conn, $query_usuario);
 
-    //Menu simples
-    echo "<a href='crud.php'>Listar</a><br>";
+    if (mysqli_affected_rows($conn)) {
+        $_SESSION['msg'] = "<p style='color: #0000ff'>Usuario Deletado com sucesso!</p>";
+        header("Location: crud.php");
+    } else {
+        $_SESSION['msg'] = "<p style='color: #f00'>Erro: Usuario não foi Deletado!</p>";
+        header("Location: crud.php");
+    }
