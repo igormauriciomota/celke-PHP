b4fe047a57c882ff63d87bde54005d22a44996a7
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-08-08 21:42:54 -0300

    Como usar o RIGHT JOIN
    A clausula RIGHT retorna todos os dados encontrados na tabela a direita de JOIN.
            Caso nao existam dados associados entre as tabelas a esquerda e a direita de JOIN, serão retornados valores nulos

--- Join/index_right.php
+++ Join/index_right.php
@@ -11,11 +11,37 @@ include_once 'conexao.php';
 </head>
 
 <body>
+    <!-- A clausula RIGHT retorna todos os dados encontrados na tabela a direita de JOIN.
+        Caso nao existam dados associados entre as tabelas a esquerda e a direita de JOIN, serão retornados valores nulos.-->
     <h1>Como usar o RIGHT JOIN</h1>
     <!-- Inicio PHP -->
     <?php
 
+    $query_usuarios = "SELECT ende.id, ende.logradouro, ende.numero, ende.bairro, ende.cidade, ende.estado,
+    usr.id, usr.nome, usr.email
+    FROM enderecos AS ende
+    RIGHT JOIN usuarios AS usr ON usr.id=ende.usuario_id ";
+    $result_usuarios = mysqli_query($conn, $query_usuarios);
 
+    while ($row_usuario = mysqli_fetch_assoc($result_usuarios)) {
+        //var_dump($row_usuario);
+        echo "<br>";
+        extract($row_usuario);
+
+        //Coluna Usuario
+        echo "Id. $id <br>";
+        echo "Nome: $nome <br>";
+        echo "E-mail: $email <br>";
+
+        //Coluna Endereço
+        echo "Id. $id <br>";
+        echo "Logradouro: $logradouro <br>";
+        echo "Nº. $numero <br>";
+        echo "Bairro: $bairro <br>";
+        echo "Cidade: $cidade <br>";
+        echo "Estado: $estado <br><br>";
+        echo "<hr>";
+    }
 
 
     ?>
