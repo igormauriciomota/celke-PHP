930c139c6ed332d885681c23b4ae9bc27b21c9f5
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-08-09 17:19:27 -0300

    Funções AVG
    Retorna o valor medio de uma coluna numerica

--- Func/index_avg.php
+++ Func/index_avg.php
@@ -12,15 +12,34 @@ include_once 'conexao.php';
 
 <body>
     <!-- Função COUNT, SUM e AVG -->
-    <h1>Função COUNT, SUM e AVG</h1>
-    <!-- Função COUNT() Retorna o numero de linhas da pesquisa->  -->
+    <h1>Função AVG</h1>
+    <!-- Função AVG() Retorna o valor medio de uma coluna numerica->  -->
 
     <!-- INICIO PHP -->
     <?php
+    //Com Casa DECIMAL
+    $query_media_preco = "SELECT AVG(preco) AS media_preco 
+    FROM inscricoes_cursos
+    WHERE curso_id = 1";
+    $result_media_preco = mysqli_query($conn, $query_media_preco);
 
+    $row_media_preco = mysqli_fetch_assoc($result_media_preco);
+    //var_dump($row_media_preco);
+    extract($row_media_preco);
 
+    echo "Média paga no curso: R$ " . number_format($media_preco, 2, ",", ".") . "<br><br>";
 
+    //Sem a Casa DECIMAL
+    $query_media_preco = "SELECT format(AVG(preco), '#') AS media_preco 
+    FROM inscricoes_cursos
+    WHERE curso_id = 1";
+    $result_media_preco = mysqli_query($conn, $query_media_preco);
 
+    $row_media_preco = mysqli_fetch_assoc($result_media_preco);
+    //var_dump($row_media_preco);
+    extract($row_media_preco);
+
+    echo "Média paga no curso: R$ " . number_format($media_preco, 2, ",", ".") . "<br>";
     ?>
     <!-- FIM PHP -->
 </body>
