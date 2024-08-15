7a63de0510ce7034d53956921c73913333ad0828
Igor Mota <107084301+igormauriciomota@users.noreply.github.com>
2024-08-15 19:49:44 -0300

    Relembrando inicio de PHP

--- PHP_Aula-1/index_01.php
+++ PHP_Aula-1/index_01.php
@@ -6,9 +6,71 @@ include_once 'conexao.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tipos de Variaveis</title>
 </head>
 <body>
+    <h1>Variaveis seus tipos de dados e referencia</h1>
+    <!-- Inicio PHP -->
+    <?php
+
+    // Variavel começa sempre co $_ ou $letras
+    // estamos grardando dentro da variavel name a string Igor
+    $name = 'Igor';
+
+    echo $name;
+
+    echo "<br><br>";
+
+    echo gettype($name);
+
+    echo "<br><br>";
+    echo "<hr>";
+    // String - tudo que colocar entre aspas simples de duplas; gettype = tipo de dado
+
+    echo gettype('Hello world');
+
+    echo "<br><br>";
     
+    //numbers - integers, float(double) gettype = tipo de dado
+     
+    echo gettype(12);
+
+    echo "<br><br>";
+
+    // float(double) - 
+    echo gettype(12.12);
+
+    echo "<br><br>";
+
+    // booleans - true ou false
+    echo gettype(false);
+    echo "<br><br>";
+
+    echo gettype(true);
+    echo "<br><br>";
+
+    // arrays Varios tipos de valores dentro do cochete [ separado por virgulas]
+    echo gettype(['Arrays', 123654]);
+
+    echo "<br><br>";
+
+    //object transforma a classe em um objeto
+    class Person {
+
+    }
+
+    echo gettype(new Person);
+
+    echo "<br><br>";
+    // null - Ausencia de um valor
+    echo gettype(null);
+
+
+
+
+
+
+    ?>
+    <!-- Fim PHP -->
 </body>
 </html>
